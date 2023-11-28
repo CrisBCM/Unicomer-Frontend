408a36f890484287334e379f634cbd41d7808aff
CrisBCM <cristianbcm1999@gmail.com>
2023-11-27 23:24:49 -0300

    Feat: Add mobile menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faCreditCard, faGift, faHandHoldingUsd, faHouse, faQuestion, faRightLeft, faShield, faSignOutAlt, faTag, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary){
+    library.addIcons(faBars, faXmark, faHouse, faCreditCard, faHandHoldingUsd, faRightLeft, faTag, faShield, faGift, faQuestion, faSignOutAlt);
+  }
+ }
